7c464024ea9748f9bf4d2438bbc3eb9b3cf5d1af
Wakaru Himura <wakaru44@gmail.com>
2024-06-29 22:47:13 +0200

    fix: fixes to my own fuckups. mostly bad calls to update and missing self on draggable pile.

--- solitaire/card.py
+++ solitaire/card.py
@@ -5,13 +5,12 @@ import flet as ft
 CARD_WIDTH = 70
 CARD_HEIGTH = 100
 DROP_PROXIMITY = 20
+CARD_OFFSET = 20
 
 
 class Card(ft.GestureDetector):
     def __init__(self, solitaire, color_name):
         super().__init__()
-        self.slot = None
-        self.solitaire = solitaire
         #
         self.mouse_cursor = ft.MouseCursor.MOVE
         self.drag_interval = 5
@@ -21,53 +20,87 @@ class Card(ft.GestureDetector):
 
         self.left = None
         self.top = None
+        self.solitaire = solitaire
+        self.slot = None
+        self.card_offset = CARD_OFFSET
+
         self.color = color_name
         self.content = ft.Container(
             bgcolor=self.color,
             width=CARD_WIDTH,
             height=CARD_HEIGTH,
         )
+        self.draggable_pile = [self]
 
     def move_on_top(self):
-        """Push card to the top of the pile while dragging."""
-        self.solitaire.controls.remove(self)
-        self.solitaire.controls.append(self)
+        """Brings draggable card pile to the top of the stack while dragging."""
+        for card in self.draggable_pile:
+            self.solitaire.controls.remove(card)
+            self.solitaire.controls.append(card)
         self.solitaire.update()
 
     def bounce_back(self):
         """Return a card to its original position."""
-        self.top = self.slot.top
-        self.left = self.slot.left
-        self.update()
+        for card in self.draggable_pile:
+            card.top = card.slot.top + card.slot.pile.index(card) * CARD_OFFSET
+            card.left = card.slot.left
+        self.solitaire.update()
 
     def place(self, slot):
-        """Snap a card into a slot."""
-        self.top = slot.top
-        self.left = slot.left
-        self.slot = slot
+        """Snap a pile of cards into a slot."""
+        for card in self.draggable_pile:
+            card.top = slot.top + len(slot.pile) * CARD_OFFSET
+            card.left = slot.left
+
+            # Remove the card from the previous slot
+            if card.slot:
+                card.slot.pile.remove(card)
+
+            # Change to new slot
+            card.slot = slot
+
+            # Add the card to the new slot's pile
+            slot.pile.append(card)
+
+        self.solitaire.update()
+
+    def get_draggable_pile(self):
+        """
+        Returns list of cards that will be dragged together starting with
+        the current card.
+        """
+        if self.slot is not None:
+            self.draggable_pile = self.slot.pile[self.slot.pile.index(self) :]
+        else:  # slot is None when the cards are dealt and need to be placed first time.
+            self.draggable_pile = [self]
 
     def start_drag(self, e: ft.DragStartEvent):
         """Start dragging a card to keep track of prev. positions."""
+        self.get_draggable_pile()
         self.move_on_top()
-        self.update()
+        self.solitaire.update()
 
     def drag(self, e: ft.DragUpdateEvent):
-        """Allow the card to be dragged around the screen."""
-        self.top = max(0, self.top + e.delta_y)
-        self.left = max(0, self.left + e.delta_x)
-        self.update()
+        """Allow the pile of cards to be dragged around the screen."""
+        for card in self.draggable_pile:
+            card.top = (
+                max(0, self.top + e.delta_y)
+                + self.draggable_pile.index(card) * CARD_OFFSET
+            )
+            card.left = max(0, self.left + e.delta_x)
+            self.solitaire.update()
 
     def drop(self, e: ft.DragEndEvent):
         """If a card is close enough to a slot, snap it into place."""
         for slot in self.solitaire.slots:
             if (
-                abs(self.top - slot.top) < DROP_PROXIMITY
+                abs(self.top - (slot.top + len(slot.pile) * CARD_OFFSET))
+                < DROP_PROXIMITY
                 and abs(self.left - slot.left) < DROP_PROXIMITY
             ):
                 self.place(slot)
-                self.update()
+                self.solitaire.update()
                 return
 
         # or bounce back
         self.bounce_back()
-        self.update()
